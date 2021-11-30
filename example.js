0a9e6d1cdb711cabcb2f954f19d8335eea75a1e1
Jonathan Starck <29304143+Psycho0verload@users.noreply.github.com>
2021-11-30 14:28:39 +0100

    Create example.js

---
+++ example.js
@@ -0,0 +1,12 @@
+/*
+Console log output
+*/
+
+var PasswordCard = require("lib/PasswordCard.js");
+var PCard = new PasswordCard("ThisIsMyCardCode", 13, 13, 'aA1!');
+
+var card = PCard.getCard(null);
+
+for (var i = 0, l = card.length; i < l; i++) {
+  console.log(card[i].join("\t"));
+}
